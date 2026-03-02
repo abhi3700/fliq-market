f562c92d8bc53421a8d0d619188803458256d7f9
Abhijit Roy <dalvath3700@gmail.com>
2026-03-02 13:35:09 +0530

    Add 'cloudflare pages function' to hide API_KEY while making request to UniFi API server
    - Fixed CORS issue.
    - No more API_KEY public exposure.
    
    Improve README:
    - refine the steps for local & production
    - ..... the notes about merchant(s).

---
+++ src/lib/unifi/constants.ts
@@ -0,0 +1,4 @@
+//
+export const UNIFI_WEB_APP_BASE_URL = "https://unifiweb3.pages.dev";
+// export const UNIFI_WEB_APP_BASE_URL = "http://0.0.0.0:3334";
+// export const UNIFI_WEB_APP_BASE_URL = "http://localhost:3334";
